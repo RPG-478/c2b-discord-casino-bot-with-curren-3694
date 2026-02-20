fcd639bba6afdff822378ed9d04fe3bb0449b986
RPG-478 <ucs250310@hs.city-saitama.ed.jp>
2026-02-20 21:19:19 +0900

    Initial commit from C2B

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+"""Flask health check for Koyeb"""
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!", 200
+
+def run():
+    app.run(host="0.0.0.0", port=5000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.daemon = True
+    t.start()
